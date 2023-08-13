ce5cf3d6c239916d306ddc5c998f85194d6a836f
rathodugam <rathodugam1@gmail.com>
2023-08-13 19:33:28 +0530

    blog slider

--- js/main.js
+++ js/main.js
@@ -5,3 +5,38 @@ $(window).scroll(function(){
     if (scroll >= 100) sticky.addClass('fixed');
     else sticky.removeClass('fixed');
   });
+
+
+  var swiper = new Swiper(".brand_slider", {
+    slidesPerView: 4,
+    spaceBetween: 30,
+    loop: true,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+  });
+
+//   var swiper1 = new Swiper(".blog_slider", {
+//     slidesPerView: 3,
+//     spaceBetween: 20,
+//     loop:true,
+//     pagination: {
+//       el: ".swiper-pagination",
+//       clickable: true,
+//     },
+//   });
+
+
+  var swiper = new Swiper(".blog_slider", {
+    slidesPerView: 3,
+    spaceBetween: 20,
+    // loop: true,
+    pagination: {
+      el: ".swiper-pagination",
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  });
